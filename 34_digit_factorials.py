39e459f31074d7f6a23717b6ade0e2540f8afe77
altbot <matthew.ault@gmail.com>
2014-01-29 22:00:38 +0000

    Add solutions to problems 30 and 34

---
+++ 34_digit_factorials.py
@@ -0,0 +1,16 @@
+# http://projecteuler.net/problem=34
+# 145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
+#
+# Find the sum of all numbers which are equal to the sum of the factorial
+# of their digits.
+# Note: as 1! = 1 and 2! = 2 are not sums they are not included.
+import math
+
+l = []
+for i in xrange(30, 1000000):
+    digits = list(str(i))
+    s = sum(math.factorial(int(d)) for d in digits)
+    if i == s:
+        l.append(i)
+        print l
+        print sum(l)
