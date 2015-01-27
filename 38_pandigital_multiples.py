e5414c73a58c9e20acc97e0b005e05d0eb93fb3e
aultimus <matthew.ault@gmail.com>
2015-01-27 21:27:25 +0000

    38 Pandigital multiples: Add solution

---
+++ 38_pandigital_multiples.py
@@ -0,0 +1,11 @@
+import utils
+
+pans = [p for p in utils.gen_pandigital_strings("123456789")]
+mult_pans = []
+
+for n in xrange(2, 20):
+    for i in xrange(1,100000):
+        concat = "".join([str(i * x) for x in xrange(1, n + 1)])
+        if len(concat) == 9 and concat in pans:
+            mult_pans.append(int(concat))
+print max(mult_pans)
