2cba7273998b1ef4e0bb0fb3cf6737be74a57989
aultimus <matthew.ault@gmail.com>
2015-02-05 16:44:14 +0000

    7 10001st prime: Tidy up solution

--- 7_10001st_prime.py
+++ 7_10001st_prime.py
@@ -2,10 +2,11 @@
 # By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see
 # that the 6th prime is 13. What is the 10 001st prime number?
 
-from utils import sieveOfEratosthenes
+import utils
 
 count = 0
-for prime in sieveOfEratosthenes(1000000):
+for prime in utils.sieveOfEratosthenes(1000000):
     count += 1
     if count == 10001:
         print prime
+        break
