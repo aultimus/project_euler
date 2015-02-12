a9ca86bc3ab0d65a576f41cfb274d512d06ee5eb
aultimus <matthew.ault@gmail.com>
2015-02-12 18:30:38 +0000

    5: Smallest Multiple: Optimisation

--- 5_smallest_multiple.py
+++ 5_smallest_multiple.py
@@ -4,8 +4,9 @@
 # What is the smallest positive number that is evenly divisible by all of the
 # numbers from 1 to 20?
 
-for i in xrange(1, 1000000000):
-    for j in xrange(1, 21):
+# 1 -> 10 are factors of 10 -> 20
+for i in xrange(20, 1000000000):
+    for j in xrange(10, 21):
         if i % j:
             break
         if j == 20:
